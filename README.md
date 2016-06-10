61fe745267ae300b95c7a9fad4eaa5b380ef5be1
Cameron Poore <campoore2@gmail.com>
2016-06-10 10:11:55 -0700

    Incorporated Bootstrap navbar and a FAQ page. It will have a FAQ telling the user what makes the input work.

---
+++ README.md
@@ -0,0 +1,43 @@
+Ping Pong
+
+#### _Input a number to receive a value or text stating "Ping", "Pong" or "Pingpong" _
+
+#### By _Cameron Poore_
+
+## Description
+
+_A website using JaveScript that returns a value or text_
+
+## Setup/Installation Requirements
+
+* Open index.html in any web browser
+
+
+## Known Bugs
+
+No current bugs
+
+## Support and contact details
+
+eMail me at campoore2@gmail.com
+
+## Technologies Used
+
+* atom text editor
+* HTML
+* CSS
+* bootstrap
+* JaveScript
+* JQuery
+
+## Specifications
+
+* Input a value to receive a number or text
+  * Example: 6 and the output is Ping
+* Any number divisible by 3, 5, or 15 will return text.
+
+### License
+
+
+
+Copyright (c) 2016 **_Cameron Poore_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,63 @@
-$(document).ready(function(){
-  $("#pinput").submit(function(event){
-    event.preventDefault();
-    var userInput = ("#ptext").val();
+// $(document).ready(function(){
+//   $("#pinput").submit(function(event){
+//     event.preventDefault();
+//     var pingPong = function()
+//     var index = 1, results=""
+//     debugger;
+//     if( index % 3 === 0){
+//       return = "ping"
+//     };
+//     if( index % 5 === 0 ){
+//       return = "pong"
+//     };
+//     if ( index % 15 === 0 ){
+//       return = "pingpong"
+//     }; var results = ("#results").append("<li>" + index +"</li>")
+//
+//     // pingPong = ("#results").append("<li>" + ("#pnum") + "</li>" )
+// });
+// })
 
-  })
-})
+
+// $(document).ready(function(){
+//   $("#pinput").submit(function(event){
+//     event.preventDefault();
+//     debugger;
+//     var pingpong = function(results){
+//     var ("#pinput") = index
+//     var index = 1;
+//     while (index <= results)
+//     if (index % 3 === 0){
+//       results += "ping"
+//     };
+//     if (index % 5 === 0){
+//       results += "pong"
+//     };
+//     return results
+//     var results = ""
+//     var results = ("#results").append("<li>" + index + "</li>")
+//   }
+//   });
+//   })
+
+
+var pingpong = function(res){
+  var results = "";
+  if(res % 3 )
+  {return "ping"};
+  if(res % 5 )
+  {return "pong"};
+  if(res % 15 )
+  {return "pingpong"};
+  return (results)
+
+  $(document).ready(function(){
+    $("#pinput").submit(function(event){
+      event.preventDefault();
+      var i = 1;
+      while (i <= $("#pnum").val()){
+        $("#results").text
+      };
+    });
+  });
+}
