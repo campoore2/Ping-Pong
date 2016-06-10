711008dcad9c18486427a0241df469c77e00f693
Cameron Poore <campoore2@gmail.com>
2016-06-10 11:12:40 -0700

    Working JS script with minor bug. It will have a fix for the bug outputting too many elements when a number is inputed.

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,26 @@ $(document).ready(function(){
   $("#pinput").submit(function(event){
     event.preventDefault();
     list = []
-    $('li.temp').remove();
-    userInput = $("#pinput").val();
+    // $('#results').remove();
+    userInput = $("#pnum").val();
     pingpong(userInput);
     });
   });
-
   var list = []
   var pingpong = function(input){
     var number = parseInt(input);
     for (var i = 1; i<=number; i++){
       if(i % 15  === 0){
       list.push("pingpong");
-    }if(i % 5 === 0 ){
+    }else if(i % 5 === 0 ){
       list.push("pong");
-    }if(i % 3 === 0){
+    }else if(i % 3 === 0){
       list.push("ping");
     }else {
       list.push(i)
     }
-    list.forEach()
-        $("ul#results").append("<li>" + item +"</li>")
-      };
+    list.forEach(function(item) {
+        $("#results").append("<li>" + item + "</li>");
+      })
     };
+  }
