dfdac66a0fde598714da94cb8ceebf849b92fe1e
Cameron Poore <campoore2@gmail.com>
2016-06-10 11:35:33 -0700

    Fixed bug in JS. Now outputs correct amount of list elements corresponding to an input value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
   $("#pinput").submit(function(event){
     event.preventDefault();
-    list = []
-    // $('#results').remove();
+    list = [];
     userInput = $("#pnum").val();
     pingpong(userInput);
     });
   });
-  var list = []
+  var list = [];
   var pingpong = function(input){
     var number = parseInt(input);
-    for (var i = 1; i<=number; i++){
+    for (i = 1; i <= number; i++)
       if(i % 15  === 0){
       list.push("pingpong");
     }else if(i % 5 === 0 ){
@@ -18,10 +17,9 @@ $(document).ready(function(){
     }else if(i % 3 === 0){
       list.push("ping");
     }else {
-      list.push(i)
+      list.push(i);
     }
     list.forEach(function(item) {
         $("#results").append("<li>" + item + "</li>");
       })
     };
-  }
